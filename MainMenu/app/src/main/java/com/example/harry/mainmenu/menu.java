70757fb0581c6fbbcbb7a06baaa7eae81665b459
hatInc <36504666+hatInc@users.noreply.github.com>
2018-02-20 19:29:03 +0000

    Add files via upload
    Additional menu page created

---
+++ MainMenu/app/src/main/java/com/example/harry/mainmenu/menu.java
@@ -0,0 +1,19 @@
+package com.example.harry.mainmenu;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
