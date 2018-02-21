f4691a378cc22dd2fb1bf5b28ad415219b565d46
hatInc <36504666+hatInc@users.noreply.github.com>
2018-02-21 15:41:47 +0000

    Add files via upload
    Create recipe addition

--- MainMenu/app/src/main/java/com/example/harry/mainmenu/maker.java
+++ MainMenu/app/src/main/java/com/example/harry/mainmenu/maker.java
@@ -9,16 +9,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class menu extends AppCompatActivity {
+public class maker extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_maker);
     }
 
-    public void addRecipe(View view){
-        Intent intent = new Intent(this, maker.class);
+    public void backMain(View view){
+        Intent intent = new Intent(this, menu.class);
         startActivity(intent);
     }
 }
---
+++ MainMenu/app/src/main/java/com/example/harry/mainmenu/search.java
@@ -0,0 +1,8 @@
+package com.example.harry.mainmenu;
+
+/**
+ * Created by Tom on 21/02/2018.
+ */
+
+public class search {
+}
