ee4cafd9d68390738bd03e8ef95d33a0627a94ab
hatInc <36504666+hatInc@users.noreply.github.com>
2018-03-06 17:56:33 +0000

    Add files via upload

--- MainMenu/app/src/main/java/com/example/harry/mainmenu/ingredient.java
+++ MainMenu/app/src/main/java/com/example/harry/mainmenu/ingredient.java
@@ -1,10 +1,12 @@
 package com.example.harry.mainmenu;
 
+import java.io.Serializable;
+
 /**
  * Created by Harry on 04/03/2018.
  */
 
-public class ingredient {
+public class ingredient implements Serializable{
     private String ingredientName = null;
 
     public String getIngredientName(){return ingredientName;}
--- MainMenu/app/src/main/java/com/example/harry/mainmenu/task.java
+++ MainMenu/app/src/main/java/com/example/harry/mainmenu/task.java
@@ -1,10 +1,12 @@
 package com.example.harry.mainmenu;
 
+import java.io.Serializable;
+
 /**
  * Created by Harry on 02/03/2018.
  */
 
-public class task {
+public class task implements Serializable{
 
     private String taskName = null;
     private String instructions = null;
